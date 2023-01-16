320744e3b2c0ba3d3f3251a1b0185d6e5b0b9f4d
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-16 05:19:37 +0300

    Add constructor with instance variables

--- person.rb
+++ person.rb
@@ -1,3 +1,7 @@
 class Person
-
+  attr_accessor :id, :name, :age
+  def initialize(id, name = "Unknown", parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
 end
