673e519c85fee8d7cb7219bd8439e144301ff12c
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-16 06:11:35 +0300

    Update person class

--- person.rb
+++ person.rb
@@ -1,9 +1,10 @@
 class Person
   attr_accessor :id, :name, :age
-  def initialize(id, name = "Unknown", parent_permission: true)
+  def initialize(id, age, name = "Unknown", parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @parent_permission = parent_permission
   end
 
   private
@@ -14,4 +15,5 @@ class Person
   public
   def can_use_services?
     is_of_age || @parent_permission
+  end
 end
