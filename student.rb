2951939ca5a123f18ef51965c47f4888d5cfd525
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-16 06:10:43 +0300

    Add a class that inherits constructor from Person class

---
+++ student.rb
@@ -0,0 +1,15 @@
+require_relative 'person'
+
+class Student < Person
+  def initialize(name, age, classroom)
+    super(name, age)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    puts '¯\(ツ)/¯'
+  end
+  
+end
+student = Student.new
+puts student.play_hooky
